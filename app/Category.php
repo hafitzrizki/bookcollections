494824a9dc5d1009649b21882edef5c7f66385b2
hafitzrizki <hafitzrizki@gmail.com>
2019-11-15 01:45:24 +0700

    create-book:create, edit, soft delete, delete permanent, status publish&draft, and trash

--- app/Category.php
+++ app/Category.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use SoftDeletes;
+
+    public function books(){
+        return $this->belongsToMany('App\Book');//handle many-to-many ke Book Model
+    }
 }
